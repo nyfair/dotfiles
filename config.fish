e63139337b4fea07f4d38de0049ee87927b2c33b
nyfair <nyfair2012@gmail.com>
2019-08-11 22:23:32 +0800

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,9 +1,7 @@
-set PATH /clang64/bin /mingw64/bin /usr/bin /opt/bin $PATH
-set MANPATH /usr/share/fish/man /usr/share/man $MANPATH
+set PATH /clang64/bin /mingw64/bin /usr/bin /opt/bin /c/Windows/system32 /c/Windows /c/Windows/system32/OpenSSH
 set -gx INFOPATH /usr/info /usr/share/info /usr/info /share/info $INFOPATH
 set -gx PKG_CONFIG_PATH /usr/lib/pkgconfig /usr/share/pkgconfig /lib/pkgconfig
-set -gx MANPATH $MANPATH
-
+set -gx MANPATH /usr/share/fish/man /usr/share/man
 set -gx SYSCONFDIR /etc
 
 set ORIGINAL_TMP $TMP
@@ -14,14 +12,3 @@ set -gx tmp (cygpath -w $ORIGINAL_TMP 2> /dev/null)
 set -gx temp (cygpath -w $ORIGINAL_TEMP 2> /dev/null)
 set -gx TMP /tmp
 set -gx TEMP /tmp
-
-if test -n $ACLOCAL_PATH
-    set -gx ACLOCAL_PATH $ACLOCAL_PATH
-end
-
-set -gx LC_COLLATE C
-for postinst in /etc/post-install/*.post
-    if test -e $postinst
-        sh -c $postinst
-    end
-end

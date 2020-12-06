f12856e862c2bae2aed27bce6a770b69cb832c47
nyfair <nyfair2012@gmail.com>
2020-12-06 22:59:01 +0800

    Update w2f.fish

--- w2f.fish
+++ w2f.fish
@@ -1,6 +1,6 @@
 function w2f
 	for i in **.wav
-		ffmpeg -i (realpath $i) -compression_level 8 (string replace '.wav' '.flac' (realpath $i))
+		ffmpeg -i (realpath $i) -compression_level 8 (string replace -r -i 'wav$' 'flac' (realpath $i)) 
 		rm -f $i
 	end
 end
